199e00cdfc89b455f7f28c8c3ee1583474f04020
Matt <matthew salt>
2016-04-29 19:46:23 +0100

    add getter example

--- ThingSpeakJavaScriptEx.js
+++ ThingSpeakJavaScriptEx.js
@@ -1,3 +1,4 @@
+
 request = require('request')
 http = require('http')
 
@@ -5,13 +6,23 @@ var url =  'https://api.thingspeak.com/update'
 request.post({
 	url:url,
 	form: {
-		api_key:"XXXXXXXXXX",
-		field1:46
+		//api_key:"NQR6662A22JQ7Q1G", // account key
+		field1:21
 	},
 	headers: {
-       'X-THINGSPEAKAPIKEY': 'XXXXXXXXXXXX'
+       'X-THINGSPEAKAPIKEY': 'CAHNOEBZ1S0VQCUH' //Channel key
     },
 }, function(error, response, body){
       console.log(body)
 });
+
+
+request.get({
+	url: 'https://api.thingspeak.com/channels/111490/feed.json',
+	headers:{
+		'X-THINGSPEAKAPIKEY':'T2DC5YYTWMH1ITYU'
+	}
+}, function(error, response, body){
+	console.log(JSON.parse(body, null, 2))
+})
 
